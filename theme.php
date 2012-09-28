39bef42e499624c774ff13c6a4b131bbc4aa5992
Tim Reynolds <tim@timothyreynolds.co.uk>
2012-09-28 07:50:25 +0100

    Corrected typos

--- theme.php
+++ theme.php
@@ -4,19 +4,18 @@ class Theme_Bootstrap extends Theme {
 
     public $name = 'Bootstrap';
     public $author = 'Tim Reynolds';
-    public $author_website = 'http://www.tomothyreynolds.co.uk/';
+    public $author_website = 'http://www.timothyreynolds.co.uk/';
     public $website = 'http://www.pyrocms.com/';
-    public $description = 'Bootstap template.';
+    public $description = 'Bootstrap template.';
     public $version = '2.0b';
     public $options = array('show_breadcrumbs' => array(
-                     	         'title' => 'Show Breadcrumbs',
+                     	    'title' => 'Show Breadcrumbs',
 							'description'   => 'Would you like to display breadcrumbs?',
 							'default'       => 'yes',
 							'type'          => 'radio',
 							'options'       => 'yes=Yes|no=No',
 							'is_required'   => TRUE)
 						);
-
 }
 
 /* End of file theme.php */
